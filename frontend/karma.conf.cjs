80386f7766c645465453820c52f91afc6d02749e
ist-h-i <86603334+ist-h-i@users.noreply.github.com>
2025-09-29 09:27:06 +0900

    Restore Angular Karma adapter

--- frontend/karma.conf.cjs
+++ frontend/karma.conf.cjs
@@ -5,12 +5,13 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false,
